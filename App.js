c8af7cb6806fb7c7e0b7145573850367b421c89f
Allen Loshon <allenlosho913@gmail.com>
2022-01-03 19:45:58 -0800

    load google font

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import WeatherWrapper from "./WeatherWrapper";
+import { useFonts } from 'expo-font';
 
 export default function App() {
+
+  const [loaded] = useFonts({
+    'Montserrat-Light': require('./assets/fonts/Montserrat-Light.ttf'),
+  });
+  
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WeatherWrapper />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
