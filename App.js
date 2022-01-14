c5d0c98810303b13969776b0a8706deeedbeabc1
Allen Loshon <allenlosho913@gmail.com>
2022-01-13 22:39:33 -0800

    Add custom font to all components

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import WeatherWrapper from "./WeatherWrapper";
 import { useFonts } from 'expo-font';
+import { setCustomText } from 'react-native-global-props';
 
 export default function App() {
 
   const [loaded] = useFonts({
     'Montserrat-Light': require('./assets/fonts/Montserrat-Light.ttf'),
   });
+  const customTextProps = { 
+    style: { 
+      fontFamily: "Montserrat-Light",
+      color: "#000",
+    }
+  };
+  
+  setCustomText(customTextProps);
   
   if (!loaded) {
     return null;
