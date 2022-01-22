aaafe4acc9697df404e8be4a3284830fb4dbd803
Allen Loshon <allenlosho913@gmail.com>
2022-01-21 23:19:36 -0800

    get data from api and more background images

--- WeatherWrapper.js
+++ WeatherWrapper.js
@@ -1,11 +1,7 @@
-import React, {useState, useEffect, useCallback} from 'react';
-import { StyleSheet, Text, View, Dimensions, Image, StatusBar, Platform, Button, SafeAreaView, TouchableOpacity, useWindowDimensions, Alert, ScrollView, ImageBackground, TouchableWithoutFeedback } from 'react-native';
-import { useDimensions } from "@react-native-community/hooks";
+import React, { useState, useEffect } from 'react';
+import { Text, Alert } from 'react-native';
 import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from "expo-location";
-const favicon = require('./assets/favicon.png');
 import axios from "axios";
-import Today from "./Today.js";
-import Forecast from "./Forecast.js";
 import TimeWrapper from "./TimeWrapper.js";
 // All weather descriptions I know:
 // There are lots of variants of rain so maybe just check if rain is listed then put rain image
@@ -27,22 +23,11 @@ const WeatherWrapper = () => {
         "thunderstorm" : "https://s.w-x.co/util/image/w/0622lightning.jpg?v=at&w=532&h=532",
         "snow" : "https://previews.123rf.com/images/tatman/tatman1702/tatman170200008/71880359-vertical-image-of-a-long-driveway-in-the-woods-covered-with-snow-.jpg",
         "fog" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr7Rnf_UTtYkqwtz7ST0UjLaf08c5iv6n4zg&usqp=CAU",
+        "clear" : "https://cdn.pixabay.com/photo/2018/08/06/22/55/sun-3588618__480.jpg",
+        "sleet" : "https://media.11alive.com/assets/WXIA/images/da0f7690-ab1b-480e-b86c-d398590a9c8f/da0f7690-ab1b-480e-b86c-d398590a9c8f_1920x1080.jpg",
+        "smoke" : "https://media.wired.com/photos/59df8cf66d52e55cdead6b14/master/w_2560%2Cc_limit/NapaFire-TA-859999422.jpg",
+        "misty" : "https://upload.wikimedia.org/wikipedia/commons/2/29/Misty_weather_over_Daqing_Pond.jpg",
     };
-
-     // Only get the data that we want
-    //  const forecastData = forecast.data.data.map(day => {
-    //     return {
-    //         high: day.high_temp,
-    //         low: day.low_temp,
-    //         date: day.datetime,
-    //         description: day.weather.description,
-    //         uv: day.uv,
-    //         icon_code: day.weather.icon,
-    //     }
-    // });
-
-    console.log("APP EXECUTED");
-    console.log(useDimensions());
 
     const [today, setToday] = useState("");
     const [forecast, setForecast] = useState("");
@@ -71,192 +56,27 @@ const WeatherWrapper = () => {
             
             const headers = {
                 'x-rapidapi-key': '3d25a73809msh92d1a102dce0e6ep1b8032jsn5208093ebcc3'
-              }
-    
-            // const currentRes = await axios.get(`https://weatherbit-v1-mashape.p.rapidapi.com/current`, {params, headers});
-            const currentRes = {
-                "data": [
-                    {
-                        "rh": 39,
-                        "pod": "d",
-                        "lon": -118.56,
-                        "pres": 973.1,
-                        "timezone": "America/Los_Angeles",
-                        "ob_time": "2022-01-05 20:10",
-                        "country_code": "US",
-                        "clouds": 25,
-                        "ts": 1641413400,
-                        "solar_rad": 547.6,
-                        "state_code": "CA",
-                        "city_name": "Stevenson Ranch",
-                        "wind_spd": 8.9,
-                        "wind_cdir_full": "north",
-                        "wind_cdir": "N",
-                        "slp": 1008.5,
-                        "vis": 3.1,
-                        "h_angle": 0,
-                        "sunset": "00:58",
-                        "dni": 839.51,
-                        "dewpt": 41.8,
-                        "snow": 0,
-                        "uv": 4.37616,
-                        "precip": 0,
-                        "wind_dir": 359,
-                        "sunrise": "15:01",
-                        "ghi": 551.53,
-                        "dhi": 100.44,
-                        "aqi": 69,
-                        "lat": 34.41,
-                        "weather": {
-                            "icon": "c01d",
-                            "code": 800,
-                            "description": "Scattered clouds"
-                        },
-                        "datetime": "2022-01-05:20",
-                        "temp": 67.7,
-                        "station": "SE756",
-                        "elev_angle": 33.03,
-                        "app_temp": 66.1
-                    }
-                ],
-                "count": 1
             }
-            // const forecastRes = await axios.get(`https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily`, {params, headers});
-            const forecastRes = [
-                {
-                    "high": 69.8,
-                    "low": 48.3,
-                    "date": "2022-01-05",
-                    "description": "Few clouds",
-                    "uv": 0.6,
-                    "icon_code": "c02d"
-                },
-                {
-                    "high": 73.3,
-                    "low": 47.4,
-                    "date": "2022-01-06",
-                    "description": "Scattered clouds",
-                    "uv": 1,
-                    "icon_code": "c02d"
-                },
-                {
-                    "high": 61.3,
-                    "low": 44.1,
-                    "date": "2022-01-07",
-                    "description": "Scattered clouds",
-                    "uv": 0.9,
-                    "icon_code": "c02d"
-                },
-                {
-                    "high": 61.1,
-                    "low": 45.5,
-                    "date": "2022-01-08",
-                    "description": "Overcast clouds",
-                    "uv": 0.5,
-                    "icon_code": "c04d"
-                },
-                {
-                    "high": 61.3,
-                    "low": 46.7,
-                    "date": "2022-01-09",
-                    "description": "Broken clouds",
-                    "uv": 0.4,
-                    "icon_code": "c03d"
-                },
-                {
-                    "high": 57.5,
-                    "low": 47,
-                    "date": "2022-01-10",
-                    "description": "Overcast clouds",
-                    "uv": 0.8,
-                    "icon_code": "c04d"
-                },
-                {
-                    "high": 57.4,
-                    "low": 46.1,
-                    "date": "2022-01-11",
-                    "description": "Broken clouds",
-                    "uv": 0.9,
-                    "icon_code": "c03d"
-                },
-                {
-                    "high": 60.2,
-                    "low": 43.6,
-                    "date": "2022-01-12",
-                    "description": "Scattered clouds",
-                    "uv": 0.9,
-                    "icon_code": "c02d"
-                },
-                {
-                    "high": 59.1,
-                    "low": 44.7,
-                    "date": "2022-01-13",
-                    "description": "Overcast clouds",
-                    "uv": 1,
-                    "icon_code": "c04d"
-                },
-                {
-                    "high": 61.4,
-                    "low": 48.4,
-                    "date": "2022-01-14",
-                    "description": "Broken clouds",
-                    "uv": 0.9,
-                    "icon_code": "c03d"
-                },
-                {
-                    "high": 57.3,
-                    "low": 52.4,
-                    "date": "2022-01-15",
-                    "description": "Overcast clouds",
-                    "uv": 1,
-                    "icon_code": "c04d"
-                },
-                {
-                    "high": 59.9,
-                    "low": 47.4,
-                    "date": "2022-01-16",
-                    "description": "Broken clouds",
-                    "uv": 1.1,
-                    "icon_code": "c03d"
-                },
-                {
-                    "high": 57.3,
-                    "low": 44.7,
-                    "date": "2022-01-17",
-                    "description": "Scattered clouds",
-                    "uv": 1.1,
-                    "icon_code": "c02d"
-                },
-                {
-                    "high": 55.8,
-                    "low": 45.3,
-                    "date": "2022-01-18",
-                    "description": "Broken clouds",
-                    "uv": 1.1,
-                    "icon_code": "c03d"
-                },
-                {
-                    "high": 61,
-                    "low": 47.6,
-                    "date": "2022-01-19",
-                    "description": "Broken clouds",
-                    "uv": 1.1,
-                    "icon_code": "c03d"
-                },
-                {
-                    "high": 60.6,
-                    "low": 47.4,
-                    "date": "2022-01-20",
-                    "description": "Clear Sky",
-                    "uv": 1.2,
-                    "icon_code": "c01d"
-                }
-            ]
+            
+            const currentRes = await axios.get(`https://weatherbit-v1-mashape.p.rapidapi.com/current`, {params, headers});
+            const forecastRes = await axios.get(`https://weatherbit-v1-mashape.p.rapidapi.com/forecast/daily`, {params, headers});
 
-            setToday(currentRes.data[0]);
-            defineBgImage(currentRes.data[0].weather.description);
-            setForecast(forecastRes);
-            console.log(today.temp)
+            // Only get the data that we want
+            const forecastData = forecastRes.data.data.map(day => {
+                return {
+                    high: day.high_temp,
+                    low: day.low_temp,
+                    date: day.datetime,
+                    description: day.weather.description,
+                    uv: day.uv,
+                    icon_code: day.weather.icon,
+                }
+            });
+
+            setToday(currentRes.data.data[0]);
+            defineBgImage(currentRes.data.data[0].weather.description);
+            setForecast(forecastData);
+
         } catch(err) {
             throw new Error(err)
         }
@@ -265,7 +85,7 @@ const WeatherWrapper = () => {
     const getPermission = async () => {
         try {
             let { status } = await requestForegroundPermissionsAsync();
-            console.log(status);
+            
             return status
         }
         catch(err) {
@@ -278,14 +98,13 @@ const WeatherWrapper = () => {
         const getCoords = async () => {
             try {
                 const permission = await getPermission();
-                console.log(permission);
 
                 if(permission !== 'granted'){
                     Alert.alert("We were denied!");
                     return
                 } 
                 const { coords } = await getCurrentPositionAsync({});
-                console.log(coords);
+                
                 test(coords.latitude, coords.longitude);
             } 
             catch(err) {
@@ -300,10 +119,10 @@ const WeatherWrapper = () => {
         return () => {};
     }, []);
     
-    while(!forecast) return <View>Loading</View>;
+    while(!forecast) return <Text>Loading</Text>;
 
     return (
-    <TimeWrapper forecast={forecast} temperature={today.temp} city={today.city_name} bgImageToday={bgImage} />
+        <TimeWrapper forecast={forecast} temperature={today.temp} description={today.weather.description} city={today.city_name} bgImageToday={bgImage} />
     )
 }
 
